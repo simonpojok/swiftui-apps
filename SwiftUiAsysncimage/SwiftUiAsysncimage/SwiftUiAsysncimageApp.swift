3c2126523001fd1349a2cea49b68565ad343d5e0
simonpojok <simonpojok@gmail.com>
2022-02-16 09:17:50 +0300

    created first application

---
+++ SwiftUiAsysncimage/SwiftUiAsysncimage/SwiftUiAsysncimageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiAsysncimageApp.swift
+//  SwiftUiAsysncimage
+//
+//  Created by Simon Peter Ojok on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiAsysncimageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
