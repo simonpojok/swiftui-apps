a3f9d7c94ad1c8cf4abc360b1da51004d65c6c4a
simonpojok <simonpojok@gmail.com>
2022-03-20 18:45:07 +0300

    finished decoder

---
+++ Africa/Africa/Extension/CodableBundleExtension.swift
@@ -0,0 +1,32 @@
+//
+//  CodableBundleExtension.swift
+//  Africa
+//
+//  Created by Simon Peter Ojok on 20/03/2022.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [CoverImage] {
+        // 1. Locate the json file
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle")
+        }
+        
+        // 2. Create a property for the data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle")
+        }
+        
+        // 3. Create a decoder
+        let decoder = JSONDecoder()
+        
+        // 4. Create a property for decoded data
+        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle")
+        }
+        // 5. Return Ready to use data
+        return loaded
+    }
+}
---
+++ Africa/Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Simon Peter Ojok on 20/03/2022.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
