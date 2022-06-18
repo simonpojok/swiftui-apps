f80aa28cd4b1e6cb3c153341793256bad2e9e92e
simonpojok <simonpojok@gmail.com>
2022-06-18 07:44:34 +0300

    created the onboarding view complete

--- Banky/Banky/Onboarding/OnboardingContainerViewController.swift
+++ Banky/Banky/Onboarding/OnboardingContainerViewController.swift
@@ -10,18 +10,22 @@ import UIKit
 class OnboardingContainerViewController: UIViewController {
     let pageViewController: UIPageViewController
     var pages = [UIViewController]()
-    var currentVC: UIViewController {
-        didSet {
-            
-        }
-    }
+    var currentVC: UIViewController
+    let closeButton = UIButton(type: .system)
+    
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
         
-        pages.append(ViewController1())
-        pages.append(ViewController2())
-        pages.append(ViewController3())
+        let page1 = OnboardingViewContrller(heroImageName: "delorean", titleText: "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in the 80s.")
+        
+        let page2 = OnboardingViewContrller(heroImageName: "world", titleText: "Move your money around the world quickly and securely.")
+        
+        let page3 = OnboardingViewContrller(heroImageName: "thumbs", titleText: "Learn more at www.bankey.com.")
+        
+        pages.append(page1)
+        pages.append(page2)
+        pages.append(page3)
         
         currentVC = pages.first!
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -33,7 +37,12 @@ class OnboardingContainerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setup()
+        style()
+        layout()
+    }
+    
+    private func setup() {
         view.backgroundColor = .systemPurple
         addChild(pageViewController)
         view.addSubview(pageViewController.view)
@@ -51,6 +60,20 @@ class OnboardingContainerViewController: UIViewController {
         
         pageViewController.setViewControllers([pages.first!], direction: .forward, animated: false, completion: nil)
         currentVC = pages.first!
+    }
+    
+    private func style() {
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.setTitle("Close", for: [])
+        closeButton.addTarget(self, action: #selector(closeTapped), for: .primaryActionTriggered)
+        view.addSubview(closeButton)
+    }
+    
+    private func layout() {
+        NSLayoutConstraint.activate([
+            closeButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            closeButton.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 2)
+        ])
     }
 }
 
@@ -84,25 +107,8 @@ extension OnboardingContainerViewController: UIPageViewControllerDataSource {
     }
 }
 
-
-    // MARK: - ViewControllers
-class ViewController1: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemRed
-    }
-}
-
-class ViewController2: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemGreen
-    }
-}
-
-class ViewController3: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+extension OnboardingContainerViewController {
+    @objc func closeTapped(_ sender: UIButton) {
+        // TODO
     }
 }
